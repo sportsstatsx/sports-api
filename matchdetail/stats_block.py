5deeec2439dc877b343fb68cd5c2ed2448492712
sportsstatsx <sportsstatsx@gmail.com>
2025-11-23 14:27:38 +0900

    Create stats_block.py

---
+++ matchdetail/stats_block.py
@@ -0,0 +1,13 @@
+# service/matchdetail/stats_block.py
+
+from typing import Any, Dict, Optional
+
+
+async def build_stats_block(header: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    """
+    TODO: match_team_stats / match_player_stats 를 기반으로
+    팀/선수 스탯을 만들어서 리턴.
+
+    1단계: 일단 None.
+    """
+    return None
