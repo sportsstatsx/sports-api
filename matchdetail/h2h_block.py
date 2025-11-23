86cff16ab6b4a4e078d0f583dc7c1116eb40596a
sportsstatsx <sportsstatsx@gmail.com>
2025-11-23 14:27:46 +0900

    Create h2h_block.py

---
+++ matchdetail/h2h_block.py
@@ -0,0 +1,13 @@
+# service/matchdetail/h2h_block.py
+
+from typing import Any, Dict, Optional
+
+
+async def build_h2h_block(header: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    """
+    TODO: H2HRepository 가 하던 맞대결/최근경기 계산을
+    서버 쿼리로 옮겨서 구현.
+
+    1단계: 일단 None.
+    """
+    return None
