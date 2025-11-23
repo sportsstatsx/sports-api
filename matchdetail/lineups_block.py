0fa4bbfb72516bbf98253dd065738d957601fdb9
sportsstatsx <sportsstatsx@gmail.com>
2025-11-23 14:27:29 +0900

    Create lineups_block.py

---
+++ matchdetail/lineups_block.py
@@ -0,0 +1,13 @@
+# service/matchdetail/lineups_block.py
+
+from typing import Any, Dict, Optional
+
+
+async def build_lineups_block(header: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    """
+    TODO: match_lineups / match_player_stats 등을 이용해서
+    선발/교체/포메이션 블록을 구성.
+
+    1단계: 일단 None.
+    """
+    return None
