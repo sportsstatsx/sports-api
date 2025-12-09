5a4ade90bab45026254d895bb95425e57443936c
sportsstatsx <sportsstatsx@gmail.com>
2025-12-09 15:56:24 +0900

    Update vip_db.py

--- vip_db.py
+++ vip_db.py
@@ -1,44 +1,38 @@
 # vip_db.py
 import os
-import psycopg2
-import psycopg2.extras
+import psycopg
+from psycopg.rows import dict_row
 
 VIP_DATABASE_URL = os.environ["VIP_DATABASE_URL"]
 
 
 def get_vip_connection():
-    """
-    VIP 전용 Postgres 연결 하나 열어주는 함수
-    사용이 끝나면 conn.close() 꼭 호출해야 함.
-    """
-    return psycopg2.connect(
-        VIP_DATABASE_URL,
-        cursor_factory=psycopg2.extras.RealDictCursor,
-    )
+    # VIP 전용 Postgres 연결
+    return psycopg.connect(VIP_DATABASE_URL, row_factory=dict_row)
 
 
 def vip_fetch_one(query: str, params=None):
     """
-    SELECT 한 줄만 가져오기
+    SELECT 한 row를 하나만 가져오는 헬퍼
     """
     conn = get_vip_connection()
     try:
         with conn.cursor() as cur:
             cur.execute(query, params or ())
             row = cur.fetchone()
-        return row
+            return row
     finally:
         conn.close()
 
 
 def vip_execute(query: str, params=None):
     """
-    INSERT / UPDATE / DELETE 용
+    INSERT / UPDATE / DELETE 용 헬퍼
     """
     conn = get_vip_connection()
     try:
         with conn.cursor() as cur:
             cur.execute(query, params or ())
-        conn.commit()
+            conn.commit()
     finally:
         conn.close()
