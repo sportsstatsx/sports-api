deff245edbbfde6b8f7d7f64926298347f72d007
sportsstatsx <sportsstatsx@gmail.com>
2026-02-11 21:11:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -74,6 +74,8 @@ from hockey.routers.hockey_notifications_router import hockey_notifications_bp
 from hockey.teamdetail.hockey_team_detail_routes import hockey_teamdetail_bp
 from hockey.leaguedetail.hockey_leaguedetail_routes import hockey_leaguedetail_bp
 
+from basketball.nba.routers.nba_fixtures_router import nba_fixtures_bp
+from basketball.nba.routers.nba_matchdetail_router import nba_matchdetail_bp
 
 
 import traceback
@@ -107,6 +109,9 @@ app.register_blueprint(hockey_standings_bp)
 app.register_blueprint(hockey_insights_bp)
 app.register_blueprint(hockey_notifications_bp)
 app.register_blueprint(hockey_teamdetail_bp)
+
+app.register_blueprint(nba_fixtures_bp)
+app.register_blueprint(nba_matchdetail_bp)
 
 
 # ─────────────────────────────────────────
@@ -2775,6 +2780,7 @@ def admin_board_delete_post(post_id: int):
 # ─────────────────────────────────────────
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
 
 
 
